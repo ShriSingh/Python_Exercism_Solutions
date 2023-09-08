8d17a76a6daeb092a81b7f6eeb0802a2947571cb
Shriyansh Singh <shriyansh.singh01@student.csulb.edu>
2023-09-07 22:21:56 -0700

    Adding an alternate solution to the solved problem

--- Raindrops.py
+++ Raindrops.py
@@ -1,6 +1,6 @@
 def convert(number):
     '''
-    Solving the function using solely conditional statements
+    Solving the problem using solely conditional statements
     '''
     # Checking if the number is divisible by 3, 5, and 7
     if number % 3 == 0 and number % 5 == 0 and number % 7 == 0:
@@ -19,3 +19,20 @@ def convert(number):
         return "Plong"
     # Returns the number in a string form if it's not divisible by 3, 5, nor 7
     return str(number)
+
+def convert2(number):
+    '''
+    Solving the problem using a dictionary and a loop 
+    '''
+    # Storing the sounds associated with their factor in a dictionary
+    factor_sounds = {3: "Pling", 5: "Plang", 7: "Plong"}
+    # Intializing the answer as a string
+    result_string = ""
+    # Writing a loop to go through the factors divisible by the number
+    for factor, sound in factor_sounds.items():
+        # Check if the number is divisible by the factor
+        if number % factor == 0:
+            # Concatenating the sound string to the result
+            result_string += sound
+    # Returning the result variable or a number as a string, if it's not divisible by 3, 5, nor 7
+    return result_string or str(number)
